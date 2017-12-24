1b85a42cae8d2b46b2a1142d894d9e928cdafea7
Zhongchao Qian <squid157@gmail.com>
2017-12-24 01:48:57 -0500

    require wrapper

--- include/bittorrent.php
+++ include/bittorrent.php
@@ -10,6 +10,7 @@ $rootpath=realpath(dirname(__FILE__) . '/..');
 //echo $rootpath;exit;
 set_include_path(get_include_path() . PATH_SEPARATOR . $rootpath);
 $rootpath .= "/";
+require_once($rootpath . 'mysqli_wrapper/mysql.php');
 include($rootpath . 'include/core.php');
 include_once($rootpath . 'include/functions.php');
 include_once($rootpath . 'include/database/database.inc');
