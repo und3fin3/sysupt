b92f8f01c78427ccfec9ea72d5d5965343a8158f
Zhongchao Qian <squid157@gmail.com>
2018-01-08 14:26:38 +0800

    change interval

--- announce.php
+++ announce.php
@@ -245,7 +245,8 @@ if ($anninterthreeage && ($anninterthree > $announce_wait) && (TIMENOW - $torren
 elseif ($annintertwoage && ($annintertwo > $announce_wait) && (TIMENOW - $torrent ['ts']) >= ($annintertwoage * 86400))
 	$real_annnounce_interval = $annintertwo;
 
-$real_annnounce_interval *= rand(950, 1300)/1000; // random interval avoid BOOM
+$real_annnounce_interval *= rand(1000, 1400)/1000; // random interval avoid BOOM
+$real_annnounce_interval = ceil($real_annnounce_interval);
 
 $peer_list = "";
 unset ( $self );
