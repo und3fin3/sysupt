f9b9b9affcb5195db75741b3cb5dd23f38854fb1
tongyifan <565813476@qq.com>
2018-05-27 00:46:03 +0800

    edit non-tjuip message

--- include/tjuip_helper.php
+++ include/tjuip_helper.php
@@ -31,7 +31,7 @@ function assert_tjuip_or_mod()
 
         if (!(get_user_class() >= UC_MODERATOR) && !check_tjuip($nip)) {
             stdhead("没有权限");
-            stdmsg("没有访问权限", "<div align=\"center\">你正在使用校外IP地址访问本站，不允许浏览本页面<hr><font size='3' color='red'>扫码调戏北洋媛(●'◡'●)</font><br><img src='images/qrcode-wx.png' width='30%'/><img src='images/qrcode-qq.png' width='30%'/><img src='images/qrcode-weibo.png' width='30%'/></div>");
+            stdmsg("没有访问权限", "<div align=\"center\">你正在使用校外IP地址访问本站，不允许浏览本页面，请尝试通过IPv6网络访问<hr><font size='3' color='red'>扫码调戏北洋媛(●'◡'●)</font><br><img src='images/qrcode-wx.png' width='30%'/><img src='images/qrcode-qq.png' width='30%'/><img src='images/qrcode-weibo.png' width='30%'/></div>");
             stdfoot();
             exit;
         }
