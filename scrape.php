7ec2806a1a97246b52465576c84b0b4c7f641ed4
tongyifan <565813476@qq.com>
2018-08-15 20:50:55 +0800

    add passkey check [#11](https://github.com/zcqian/tjupt/pull/11)

--- scrape.php
+++ scrape.php
@@ -8,8 +8,20 @@ block_browser();
 
 preg_match_all('/info_hash=([^&]*)/i', $_SERVER["QUERY_STRING"], $info_hash_array);
 
+// Check passkey
+$passkey = $_GET['passkey'];
+if (strlen($passkey) != 32) {
+    err("Passkey invalid.");
+} elseif (!$az = $Cache->get_value('user_passkey_' . $passkey . '_content')) {
+    $res = sql_query("SELECT id, username, downloadpos, enabled, uploaded, downloaded, class, parked, clientselect, showclienterror, enablepublic4 FROM users WHERE passkey='" . mysql_real_escape_string($passkey) . "' LIMIT 1");
+    $az = mysql_fetch_array($res);
+    $Cache->cache_value('user_passkey_' . $passkey . '_content', $az, 950);
+}
+
 if (count($info_hash_array[1]) < 1) {
     err("Empty info_hash is not allowed.");
+} elseif (!$az) {
+    err("Passkey invalid.");
 } else {
     $query = "SELECT info_hash, times_completed, seeders, leechers FROM torrents WHERE " . hash_where_arr('info_hash', $info_hash_array[1]);
     $res = sql_query($query);
