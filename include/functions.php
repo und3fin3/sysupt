1c11b05614f506b8d7a5f7182df0c1c985d37773
tongyifan <565813476@qq.com>
2018-11-29 18:18:08 +0800

    update dependency (youzan-sdk)

--- include/functions.php
+++ include/functions.php
@@ -6454,7 +6454,7 @@ function check_password($password, $user_id = null)
 }
 
 function youzan_request($method, $params = [], $files = [], $version = '3.0.0'){
-    require(dirname(FILE).'/vendor/autoload.php');
+    require($rootpath . '/vendor/autoload.php');
     global $youzan_client_id, $youzan_client_secret, $youzan_kdt_id;
     $youzan = new \Hanson\Youzan\Youzan([
         'client_id' => $youzan_client_id,
