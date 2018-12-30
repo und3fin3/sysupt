910d1316662c02db6021a8240cf136296a3cb35a
tongyifan <565813476@qq.com>
2018-12-30 11:20:29 +0800

    takeqqid API

--- api_takeqqid.php
+++ api_takeqqid.php
@@ -7,6 +7,9 @@
  * Time: 上午12:14
  */
 
+require_once("include/bittorrent.php");
+require_once("api/API_Response.php");
+dbconn();
 $token = htmlspecialchars($_POST['token']);
 $sign = htmlspecialchars($_POST['sign']);
 $qq = htmlspecialchars($_POST['qq']);
@@ -18,19 +21,19 @@ if (!auth_token($token, $sign, $username . $qq)) {
 $dupe_users = array();
 $res = sql_query("SELECT username FROM users WHERE qq = " . sqlesc($qq) . " OR email LIKE '%$qq%' OR username LIKE '%$qq%'") or sqlerr(__FILE__, __LINE__);
 while ($arr = mysql_fetch_array($res)) {
-    $dupe_users[] = $arr['username'];
+    if ($arr['username'] != $username)
+        $dupe_users[] = $arr['username'];
 }
 
 if (!empty($dupe_users)) {
-    foreach ($dupe_users as $dupe_user) {
-        if ($dupe_user != $username) {
-            $subject = "QQ群内可疑用户";
-            $msg = "申请者站内用户名：$username\n\t申请者QQ号：$qq\n\t存疑用户：{$dupe_user}";
-            sql_query("INSERT INTO staffmessages (sender, added, msg, subject) VALUES(10, " . sqlesc(date("Y-m-d H:i:s")) . ", " . sqlesc($message) . ", " . sqlesc($subject) . ")") or sqlerr(__FILE__, __LINE__);
-            $Cache->delete_value('staff_message_count');
-            $Cache->delete_value('staff_new_message_count');
-        }
-    }
-}
+    $subject = "QQ群内可疑用户";
+    $msg = "申请者站内用户名：$username\n\t申请者QQ号：$qq\n\t存疑用户：" . implode("、", $dupe_users);
+    sql_query("INSERT INTO staffmessages (sender, added, msg, subject) VALUES(10, " . sqlesc(date("Y-m-d H:i:s")) . ", " . sqlesc($message) . ", " . sqlesc($subject) . ")") or sqlerr(__FILE__, __LINE__);
+    $Cache->delete_value('staff_message_count');
+    $Cache->delete_value('staff_new_message_count');
+    $resp = new API_Response($dupe_users, -1, "Found duplicated user.");
+} else
+    $resp = new API_Response("", 0, "Not found duplicated user.");
 
 sql_query("UPDATE users SET qq = " . sqlesc($qq) . " WHERE username = " . sqlesc($username)) or sqlerr(__FILE__, __LINE__);
+echo json_encode($resp);
