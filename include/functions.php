b19dfdcb023d229530090d5a8c33e110981bb373
tongyifan <565813476@qq.com>
2019-04-01 14:43:15 +0800

    fix IMDb id pattern

--- include/functions.php
+++ include/functions.php
@@ -1676,7 +1676,7 @@ function get_torrent_extinfo_identifier($torrentid) {
 	return $result;
 }
 function parse_imdb_id($url) {
-	if ($url != "" && preg_match ( "/[0-9]{7}/i", $url, $matches )) {
+	if ($url != "" && preg_match ( "/[0-9]{7,8}/i", $url, $matches )) {
 		return $matches [0];
 	} elseif ($url && is_numeric ( $url ) && strlen ( $url ) < 7) {
 		return str_pad ( $url, 7, '0', STR_PAD_LEFT );
